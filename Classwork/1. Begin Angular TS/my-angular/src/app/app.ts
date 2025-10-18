b84a5ece010ece1d52bfbc5a0c56e798784fd4bf
Aleksandruk2 <aaleksandruk2018@gmail.com>
2025-10-18 18:42:27 +0300

    1. Angular class/home-work

---
+++ Classwork/1. Begin Angular TS/my-angular/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('my-angular');
+
+  // public counter = 0;
+  //
+  // incremeant() {
+  //   this.counter++;
+  // }
+}
